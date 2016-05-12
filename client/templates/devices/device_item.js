0acc4af158912707b1f9e0b08519752c36897904
hgdphg <hgdphg@gmail.com>
2016-05-12 23:41:31 +0700

    add style for light

--- client/templates/devices/device_item.js
+++ client/templates/devices/device_item.js
@@ -4,6 +4,14 @@ Template.deviceItem.helpers({
   }
 });
 
+Template.deviceItem.extraColor = function () {
+    if (this.message) {
+        return "background: #555555 "; // you will also need to add a 'theColorGreen' class to your .css file that matches this
+    }
+
+    return "";
+};
+
 Template.deviceItem.events({
   'click #value_on' (){
     Devices.insert({ topic: this.topic, message: "1", broadcast: true });
