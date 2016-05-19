c9f31790d87f62e4803ebb213c2da4ca16d3b23e
hgdphg <hgdphg@gmail.com>
2016-05-19 15:44:29 +0700

    Add image and config new acc

--- server/fixtures.js
+++ server/fixtures.js
@@ -3,15 +3,29 @@ if (Devices.find().count() === 0) {
   Devices.insert({
     title: 'Light 1',
     type: 1,
-    infomation: 'Den phong khach',
-    topic: "value",
+    infomation: 'LED1',
+    topic: "LED1",
     message: 1
   });
   Devices.insert({
     title: 'Light 2',
     type: 1,
-    infomation: 'Den phong ngu',
-    topic: "light",
+    infomation: 'LED2',
+    topic: "LED2",
+    message: 1
+  });
+  Devices.insert({
+    title: 'Light 3',
+    type: 1,
+    infomation: 'LED3',
+    topic: "LED3",
+    message: 1
+  });
+  Devices.insert({
+    title: 'Light 4',
+    type: 1,
+    infomation: 'LED4',
+    topic: "LED4",
     message: 1
   });
 }
--- server/main.js
+++ server/main.js
@@ -2,6 +2,6 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup\
-  Devices.mqttConnect("mqtt://jreebqah:RzEhoQNDZBiy@m21.cloudmqtt.com:10697", ['value','light']);
-
+  //Devices.mqttConnect("mqtt://jreebqah:RzEhoQNDZBiy@m21.cloudmqtt.com:10697", ['value','light']);
+  Devices.mqttConnect("mqtt://qrgrhtfl:EgXj4j87F8-M@m10.cloudmqtt.com:17877", ['LED1','LED2','LED3','LED4']);
 });
